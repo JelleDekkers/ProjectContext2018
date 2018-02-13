b2409d03186f88dc0a4d37ce550bedae55f690db
JelleDekkers <jelle.dekkers@hotmail.com>
2018-02-13 11:48:04 +0100

    Added placeholder buildings and automated building size calculator

--- ProjectContext2018Unity/Assets/Scripts/City View/Building.cs
+++ ProjectContext2018Unity/Assets/Scripts/City View/Building.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Building : MonoBehaviour {
 
-    public IntVector2 size;
+    public IntVector2 Size { get; private set; }
+
+    private void Start() {
+        Size = CalculateSize(this);
+    }
 
     public bool CanBeBought() {
         throw new System.NotImplementedException();
+    }
+
+    public static IntVector2 CalculateSize(Building b) {
+        IntVector2 calcSize = IntVector2.Zero;
+        Renderer r = b.GetComponent<Renderer>();
+        calcSize.x = (int)r.bounds.size.x;
+        calcSize.z = (int)r.bounds.size.z;
+        return calcSize;
     }
 }
