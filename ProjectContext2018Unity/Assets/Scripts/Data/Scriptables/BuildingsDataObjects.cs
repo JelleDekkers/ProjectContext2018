605bfa82dc29f2e290ad51609b9fa9d8c47cb1fb
JelleDekkers <jelle.dekkers@hotmail.com>
2018-02-17 10:09:42 +0100

    Added fallback options for resource and building data

--- ProjectContext2018Unity/Assets/Scripts/Data/Scriptables/BuildingsDataObjects.cs
+++ ProjectContext2018Unity/Assets/Scripts/Data/Scriptables/BuildingsDataObjects.cs
@@ -5,14 +5,18 @@ using CityView;
 [CreateAssetMenu(fileName = "Building Prefabs", menuName = "Tools/Building Prefabs", order = 3)]
 public class BuildingPrefabs : ScriptableObject {
 
+    public Building errorBuilding;
     public Building[] buildings;
+
+    public Sprite errorImg;
     public Sprite[] images;
 
     public Building GetBuilding(int index) {
         try {
             return buildings[index];
         } catch(Exception ex) {
-            throw ex;
+            Debug.LogError(ex);
+            return errorBuilding;
         }
     }
 
@@ -20,7 +24,8 @@ public class BuildingPrefabs : ScriptableObject {
         try {
             return images[index];
         } catch (Exception ex) {
-            throw ex;
+            Debug.LogError(ex);
+            return errorImg;
         }
     }
 }
--- ProjectContext2018Unity/Assets/Scripts/Data/Scriptables/GameResourcesDataObjects.cs
+++ ProjectContext2018Unity/Assets/Scripts/Data/Scriptables/GameResourcesDataObjects.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Resource Prefabs", menuName = "Tools/Resource Prefabs", order = 1)]
 public class GameResourcePrefabs : ScriptableObject {
 
+    public Sprite errorImg;
     public Sprite[] images;
 
     public Sprite GetSprite(int index) {
         try {
             return images[index];
         } catch (Exception ex) {
-            throw ex;
+            Debug.LogError(ex);
+            return errorImg;
         }
     }
 }
