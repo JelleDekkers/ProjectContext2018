6dbe5a5cec7b4ce219410fcf7a98a3a1abbdcbc0
Cheezegami <superronald@live.nl>
2018-02-19 12:27:01 +0100

    Old Assets for Testing

---
+++ ProjectContext2018Unity/Assets/Scripts/UI/BuildingSelector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace CompanyView {
+
+    public static class BuildingSelector {
+
+        private static GameObject selectedBuilding;
+
+        private static BuildingContainer oldBuilding;
+
+        public static GameObject SelectedBuilding {
+            get {
+                return selectedBuilding;
+            }
+
+            private set {
+                selectedBuilding = value;
+            }
+        }
+
+        public static Action OnBuildingSelected;
+
+        public static void SetBuilding( BuildingContainer buildingContainer ) {
+            if (OnBuildingSelected != null)
+                OnBuildingSelected.Invoke();
+
+            if(oldBuilding != null)oldBuilding.selected = false;
+            
+            if (SelectedBuilding == buildingContainer.building) {
+                SelectedBuilding = null;
+                buildingContainer.selected = false;
+            }
+            else {
+                SelectedBuilding = buildingContainer.building;
+                buildingContainer.selected = true;
+            }
+            oldBuilding = buildingContainer;
+        }
+
+        public static void SetToNull() {
+            SelectedBuilding = null;
+            oldBuilding = null;
+        }
+    }
+}
---
+++ ProjectContext2018Unity/Assets/Scripts/UI/Testing/TestShowBuilding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CompanyView.UI.Testing {
+    public class TestShowBuilding : MonoBehaviour {
+        private Text text;
+
+        public void Start() {
+            text = gameObject.GetComponent<Text>();
+        }
+
+        public void Update() {
+            /*if (BuildingSelector.SelectedBuilding != null) {
+
+                text.text = BuildingSelector.SelectedBuilding.name;
+            }
+            else {
+                text.text = "No building selected";
+            }*/
+        }
+    }
+}
