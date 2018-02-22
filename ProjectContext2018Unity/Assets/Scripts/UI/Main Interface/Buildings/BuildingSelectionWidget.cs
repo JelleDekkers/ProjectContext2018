b83df7938e6e31d45e5900c54e9588034934ac56
JelleDekkers <jelle.dekkers@hotmail.com>
2018-02-22 18:42:43 +0100

    Added tooltip widget on resources

---
+++ ProjectContext2018Unity/Assets/Scripts/UI/Main Interface/Buildings/BuildingSelectionWidget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CityView.UI {
+
+    public class BuildingSelectionWidget : MonoBehaviour {
+
+        
+
+        private void Start() {
+            
+        }
+    }
+}
--- ProjectContext2018Unity/Assets/Scripts/UI/Main Interface/Resources/ResourcesWidgetItem.cs
+++ ProjectContext2018Unity/Assets/Scripts/UI/Main Interface/Resources/ResourcesWidgetItem.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace CityView.UI {
 
-    public class ResourcesWidgetItem : MonoBehaviour {
+    public class ResourcesWidgetItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler { 
 
         [SerializeField]
         private int resourceID;
@@ -14,17 +15,31 @@ namespace CityView.UI {
         [SerializeField]
         private Text amountText;
 
+        private string tooltipText;
+        private RectTransform rect;
+        private readonly Vector3 tooltipOffset = new Vector3(0, -0.5f, 0);
+
         private void Start() {
             img.sprite = DataManager.ResourcePrefabs.GetSprite(resourceID);
             string resourceName = DataManager.ResourcesData.dataArray[resourceID].Name;
             float amount = PlayerResources.Resources[resourceName];
             amountText.text = Mathf.RoundToInt(amount).ToString();
             PlayerResources.OnResourceChanged += UpdateAmount;
+            tooltipText = resourceName;
+            rect = GetComponent<RectTransform>();
         }
 
         private void UpdateAmount(int id, float newAmount) {
             if(id == resourceID)
                 amountText.text = Mathf.RoundToInt(newAmount).ToString(); 
+        }
+
+        public void OnPointerEnter(PointerEventData eventData) {
+            TooltipWidget.Instance.Show(tooltipText, rect.position, tooltipOffset);
+        }
+
+        public void OnPointerExit(PointerEventData eventData) {
+            TooltipWidget.Instance.Hide();
         }
     }
 }
