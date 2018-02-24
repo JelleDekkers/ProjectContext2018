d6b580cbb26852da5060ccd0f957ddc4db274c8b
JelleDekkers <jelle.dekkers@hotmail.com>
2018-02-24 15:03:10 +0100

    Added zoom in/out

--- ProjectContext2018Unity/Assets/Scripts/City View/CityCameraController.cs
+++ ProjectContext2018Unity/Assets/Scripts/City View/CityCameraController.cs
@@ -2,29 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CityCameraController : MonoBehaviour {
+namespace CityView {
 
-    public float movementSpeed = 1f;
+    public class CityCameraController : MonoBehaviour {
 
-    [SerializeField]
-    private float horizontalBorderMin, horizontalBorderMax;
-    [SerializeField]
-    private float verticalBorderMin, verticalBorderMax;
+        public float movementSpeed = 1f;
 
-    private void Update() {
-        Movement();
-    }
+        [SerializeField]
+        private float horizontalBorderMin, horizontalBorderMax;
+        [SerializeField]
+        private float verticalBorderMin, verticalBorderMax;
+        [SerializeField]
+        private float zoomMin, zoomMax;
 
-    private void Movement() {
-        if (Input.GetMouseButton(1)) {
-            float horizontal = Input.GetAxis("Mouse X") / 2 + Input.GetAxis("Mouse Y") / 2;
-            float vertical = Input.GetAxis("Mouse X") / 2 - Input.GetAxis("Mouse Y") / 2;
-            transform.Translate(-horizontal * movementSpeed * Time.deltaTime, 0, -vertical * movementSpeed * -Time.deltaTime, Space.World);
+        private void Update() {
+            Movement();
+            Zoom();
+        }
 
-            // border:
-            float horizontalPositionClamped = Mathf.Clamp(transform.position.x, horizontalBorderMin, horizontalBorderMax);
-            float verticalPositionClamped = Mathf.Clamp(transform.position.z, verticalBorderMin, verticalBorderMax);
-            transform.position = new Vector3(horizontalPositionClamped, transform.position.y, verticalPositionClamped);
+        private void Movement() {
+            if (Input.GetMouseButton(1)) {
+                float horizontal = Input.GetAxis("Mouse X") / 2 + Input.GetAxis("Mouse Y") / 2;
+                float vertical = Input.GetAxis("Mouse X") / 2 - Input.GetAxis("Mouse Y") / 2;
+                transform.Translate(-horizontal * movementSpeed * Time.deltaTime, 0, -vertical * movementSpeed * -Time.deltaTime, Space.World);
+
+                // border:
+                float horizontalPositionClamped = Mathf.Clamp(transform.position.x, horizontalBorderMin, horizontalBorderMax);
+                float verticalPositionClamped = Mathf.Clamp(transform.position.z, verticalBorderMin, verticalBorderMax);
+                transform.position = new Vector3(horizontalPositionClamped, transform.position.y, verticalPositionClamped);
+            }
+        }
+
+        private void Zoom() {
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - Input.GetAxis("Mouse ScrollWheel"), zoomMin, zoomMax);
         }
     }
 }
