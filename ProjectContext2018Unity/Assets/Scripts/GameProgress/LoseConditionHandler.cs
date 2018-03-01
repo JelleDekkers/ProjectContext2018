f361d306852f6576d3457e47ca8344c1901e4d92
Cheezegami <superronald@live.nl>
2018-03-01 16:31:55 +0100

    event implementation start

---
+++ ProjectContext2018Unity/Assets/Scripts/GameProgress/LoseConditionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseConditionHandler : MonoBehaviour {
+    float polutionLimit = 40;
+
+	// Use this for initialization
+	void Start () {
+        PlayerResources.OnResourceChanged += Lose;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Lose(int a, int b)
+    {
+
+    }
+}
