101cc97c100f7841b7bac710041ad63b9c8ba871
Cheezegami <superronald@live.nl>
2018-03-02 13:42:09 +0100

    Basic Win Condition Framework

---
+++ ProjectContext2018Unity/Assets/Scripts/GameProgress/WinConditionHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinConditionHandler : MonoBehaviour {
+    
+	// Use this for initialization
+	private void Start () {
+    }
+
+    // Update is called once per frame
+    private void Update () {
+		
+	}
+
+    private void CheckWinCondition() {
+        if(/*WIN CONDITIONS MET*/true) {
+            Debug.Log("GAME WON");
+            // IMPLEMENT WIN GAME METHOD HERE
+        }
+    }
+}
