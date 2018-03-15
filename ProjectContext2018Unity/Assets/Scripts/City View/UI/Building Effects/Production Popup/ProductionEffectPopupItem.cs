8eca6e4cb84da246a0dc4156a5e363cdb6340b0a
JelleDekkers <jelle.dekkers@hotmail.com>
2018-03-15 20:15:36 +0100

    Added color to production pop up amount text

--- ProjectContext2018Unity/Assets/Scripts/City View/UI/Building Effects/Production Popup/ProductionEffectPopupItem.cs
+++ ProjectContext2018Unity/Assets/Scripts/City View/UI/Building Effects/Production Popup/ProductionEffectPopupItem.cs
@@ -9,11 +9,21 @@ namespace CityView.UI {
 
         [SerializeField] private Image productImg;
         [SerializeField] private Text amountTxt;
+        [SerializeField] private Color plusColor = Color.green;
+        [SerializeField] private Color minColor = Color.red;
 
         public void Init(Sprite productSprite, float amount) {
             productImg.sprite = productSprite;
             //amountTxt.text = (amount > 0) ? "+" : "-";
             amountTxt.text = Mathf.RoundToInt(amount).ToString();
+
+            if (amount >= 0)
+                amountTxt.text = "+" + amountTxt.text;
+
+            if (amount >= 0)
+                amountTxt.color = plusColor;
+            else
+                amountTxt.color = minColor;
         }
     }
 }
