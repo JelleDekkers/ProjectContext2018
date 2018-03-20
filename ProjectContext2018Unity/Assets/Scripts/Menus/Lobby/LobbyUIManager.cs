454f8b00683ab294bf7dc99632b28996eeacb445
JelleDekkers <jelle.dekkers@hotmail.com>
2018-03-20 18:52:16 +0100

    Added placeholder objective info in the lobby scene

---
+++ ProjectContext2018Unity/Assets/Scripts/Menus/Lobby/LobbyUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LobbyUIManager : MonoBehaviour {
+
+    [SerializeField] private Text infoTxt;
+    [SerializeField] private string serverInfoTxt, clientInfoTxt;
+
+    private void Awake() {
+        if (NetworkManager.singleton.numPlayers == 1)
+            infoTxt.text = serverInfoTxt;
+        else
+            infoTxt.text = clientInfoTxt;
+    }
+}
