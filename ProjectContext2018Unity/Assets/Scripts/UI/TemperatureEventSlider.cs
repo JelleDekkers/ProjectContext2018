d84e9db505675825814b1b8a308429c4e05bbb8a
JelleDekkers <jelle.dekkers@hotmail.com>
2018-03-23 14:07:36 +0100

    Added temperature widget event sliders that show at what degrees the next event is

---
+++ ProjectContext2018Unity/Assets/Scripts/UI/TemperatureEventSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemperatureEventSlider : MonoBehaviour {
+
+	public Text temperatureTxt;
+    public Slider slider;
+
+    public void Init(float temperature, float min, float max) {
+        temperatureTxt.text = string.Format("{0:0.00}", temperature.ToString());
+        slider.minValue = min;
+        slider.maxValue = max;
+        slider.value = temperature;
+    }
+}
--- ProjectContext2018Unity/Assets/Scripts/UI/TemperatureWidget.cs
+++ ProjectContext2018Unity/Assets/Scripts/UI/TemperatureWidget.cs
@@ -5,17 +5,45 @@ using UnityEngine.UI;
 
 public class TemperatureWidget : MonoBehaviour {
 
+    [SerializeField] private TemperatureEventsManager eventsManager;
     [SerializeField] private Slider slider;
     [SerializeField] private WorldTemperature temperature;
     [SerializeField] private Text temperatureText;
+    [SerializeField] private TemperatureEventSlider eventSlider;
+    [SerializeField] private Transform eventSlidersParent;
+
+    private TemperatureEventSlider[] sliders;
 
     private void Start() {
         slider.maxValue = temperature.MaxTemperature;
         slider.minValue = temperature.StartingTemperature;
+
+        sliders = new TemperatureEventSlider[eventsManager.events.Length];
+        for(int i = 0; i < eventsManager.events.Length; i++) {
+            sliders[i] = InstantiateEventSliders(eventsManager.events[i]);
+        }
+
+        TemperatureEventsManager.OnEventTriggered += RemoveSlider;
+    }
+
+    private void OnDestroy() {
+        TemperatureEventsManager.OnEventTriggered -= RemoveSlider;
     }
 
     private void Update() {
         slider.value = temperature.CurrentTemperature;
         temperatureText.text = ((int)(temperature.CurrentTemperature)).ToString() + "C";
+    }
+
+    private void RemoveSlider(float newWaterLevel) {
+        Destroy(sliders[eventsManager.currentEventIndex].gameObject);
+    }
+
+    private TemperatureEventSlider InstantiateEventSliders(TemperatureEvent e) {
+        TemperatureEventSlider slider = Instantiate(eventSlider);
+        slider.transform.SetParent(eventSlidersParent, false);
+        slider.transform.SetAsFirstSibling();
+        slider.Init(e.temperatureTrigger, temperature.StartingTemperature, temperature.MaxTemperature);
+        return slider;
     }
 }
