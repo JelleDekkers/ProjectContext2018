61f6b412a07219a6130eeb3af1307a1580c13208
JelleDekkers <jelle.dekkers@hotmail.com>
2018-03-27 22:22:10 +0200

    Added tutorial components

---
+++ ProjectContext2018Unity/Assets/Scripts/Tutorial/TutorialItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialItem : MonoBehaviour {
+
+    public UnityEvent onItemReached;
+
+    public void Start() {
+        onItemReached.Invoke();
+    }
+}
---
+++ ProjectContext2018Unity/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    public TutorialItem[] items;
+    public bool playTutorial;
+
+    private TutorialItem currenItem;
+    private int index = 0;
+
+    private void Start() {
+        items = GetComponentsInChildren<TutorialItem>(true);
+        currenItem = items[0];
+        currenItem.gameObject.SetActive(true);
+    }
+
+    public void StopTutorial() {
+        gameObject.SetActive(false);
+    }
+
+    public void NextTutorialItem() {
+        if (index < items.Length - 1) {
+            currenItem.gameObject.SetActive(false);
+            index++;
+            currenItem = items[index];
+            currenItem.gameObject.SetActive(true); 
+        } else {
+            StopTutorial();
+        }
+    }
+}
