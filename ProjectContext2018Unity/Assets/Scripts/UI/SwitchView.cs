9cf90db079b879d11820a44abd30abb27bb22a9a
Cheezegami <superronald@live.nl>
2018-03-28 16:26:43 +0200

    More Sounds

--- ProjectContext2018Unity/Assets/Scripts/UI/SwitchView.cs
+++ ProjectContext2018Unity/Assets/Scripts/UI/SwitchView.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 public class SwitchView : MonoBehaviour {
-
+    public bool earthActive;
     public static Action OnSceneViewSwitched;
 
     public GameObject cityView, earthView;
@@ -10,6 +10,12 @@ public class SwitchView : MonoBehaviour {
     public void ToggleView() {
         earthView.SetActive(!earthView.activeInHierarchy);
         cityView.SetActive(!cityView.activeInHierarchy);
+        if(earthView.activeInHierarchy) {
+            earthActive = true;
+        }
+        else {
+            earthActive = false;
+        }
 
         //if (OnSceneViewSwitched != null)
         //    OnSceneViewSwitched();
---
+++ ProjectContext2018Unity/Assets/ZoomSoundSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomSoundSwitcher : MonoBehaviour {
+    private SwitchView view;
+    private AudioSource source;
+
+    public AudioClip zoomOutSound;
+    public AudioClip zoomInSound;
+	// Use this for initialization
+	void Start () {
+        view = GetComponent<SwitchView>();
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleSound() {
+        if(view.earthActive) {
+            source.PlayOneShot(zoomOutSound);
+        }
+        else {
+            source.PlayOneShot(zoomInSound);
+        }
+    }
+}
