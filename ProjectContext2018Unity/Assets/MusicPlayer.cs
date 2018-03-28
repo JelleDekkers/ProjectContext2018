326291409283bc746ff23ebc68ffd0f14d55e366
Cheezegami <superronald@live.nl>
2018-03-28 15:47:44 +0200

    audio source edited

---
+++ ProjectContext2018Unity/Assets/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+    public AudioClip menuClip;
+    public AudioClip gameClip;
+
+    private AudioSource source;
+	// Use this for initialization
+	private void Start () {
+        source = GetComponent<AudioSource>();
+        DontDestroyOnLoad(this);
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+	
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        if(scene.buildIndex == 0 || scene.buildIndex == 1) {
+            if (source.clip != menuClip) {
+                source.clip = menuClip;
+                source.Play();
+            }
+        }
+        else {
+            if (source.clip != gameClip) {
+                source.clip = gameClip;
+                source.Play();
+            }
+        }
+
+    }
+    
+}
